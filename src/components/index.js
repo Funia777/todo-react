2dfe9f4fe0da9e369bcce50700c6a574499d822d
Funia777 <Kirill>
2021-11-13 13:47:14 +0300

    11

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as List } from './List/index.jsx'
-export { default as AddListButton } from './AddlistButton/AddButtonList.jsx'
+export { default as AddListButton } from './AddlistButton/index.jsx'
 export { default as Tasks } from './Tasks/index.jsx'
 export { default as Circle } from './Circle/index.jsx'
